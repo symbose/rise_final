c1e95243719671ef0d0a5e8418388b22aff6fddb
Apple <apple@Apples-MacBook-Pro-9.local>
2019-07-17 16:31:09 -0700

    day 8 push

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 
-var button = $(`button`);
+var button = $(`#kay`);
+var button_one = $(`#first`);
 
 button.on("click", clicked);
 function clicked(){
+  window.location.href = "./main.html";
+
+}
+
+button_one.on("click", ones);
+function ones(){
+  window.location.href = "./aboutus.html";
+
+}
