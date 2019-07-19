98a4d12d5036d5e8317cf8ed11c100b455a2fc12
Apple <apple@Apples-MacBook-Pro-9.local>
2019-07-19 08:22:22 -0700

    final day push1

--- main.js
+++ main.js
@@ -1,9 +1,49 @@
+// var button = $(`#add`);
+// var waterbottle = $(`#one`);
+// var straw = $(`#two`);
+// var plastic = $(`#three`);
+// var foodWaste = $(`#four`);
+// // var showimage = $(``);
+// var addPoints = 0;
+//
+// button.on("click", add);
+//
+// function add (){
+//   event.preventDefault()
+//
+//
+//   if (waterbottle.prop("checked") == true){
+//     addPoints += 5;
+//   }
+//   if (straw.prop("checked") == true){
+//     addPoints += 5;
+//   }
+//   if (plastic.prop("checked") == true){
+//     addPoints += 5;
+//   }
+//   if (foodWaste.prop("checked") == true){
+//     addPoints += 5;
+//   }
+//
+//   $("#points").text(addPoints);
+//
+//   if (addPoints == 15){
+//       toggleclass
+//   }
+//
+//
+//
+// }
+
 var button = $(`#add`);
 var waterbottle = $(`#one`);
 var straw = $(`#two`);
 var plastic = $(`#three`);
 var foodWaste = $(`#four`);
-var showimage =
+var lights = $(`#five`);
+var clothes = $(`#six`);
+var containers = $(`#seven`);
+var litter = $(`#eight`);
 
 button.on("click", add);
 
@@ -21,6 +61,21 @@ function add (){
     addPoints += 5;
   }
   if (foodWaste.prop("checked") == true){
+    addPoints += 5;
+  }
+   if (lights.prop("checked") == true){
+    addPoints += 5;
+  }
+   if (clothes.prop("checked") == true){
+    addPoints += 5;
+  }
+   if (containers.prop("checked") == true){
+    addPoints += 5;
+  }
+   if (foodWaste.prop("checked") == true){
+    addPoints += 5;
+  }
+   if (litter.prop("checked") == true){
     addPoints += 5;
   }
 
